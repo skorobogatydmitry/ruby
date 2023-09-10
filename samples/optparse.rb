513c2e8700aa0f4174e580b1e8bbfd5fcabde5fd
decay <decayhonor@gmail.com>
2023-09-10 11:28:35 +0300

    add optparse example

---
+++ samples/optparse.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'optparse'
+
+options = {
+  verbose: false # default value
+}
+
+OptionParser.new { |opts|
+  opts.banner = <<~EOD
+    OptParse examples.
+    Usage: #{$0} [args]
+    0. This message is a nice multi-line help.
+    1. Boolean flag parsing
+      Source: https://stackoverflow.com/questions/54576873/ruby-optionparser-short-code-for-boolean-option
+      Behavior:
+        - ./optparse.rb --verbose    => true
+        - ./optparse.rb --verbose no => false
+        - ./optparse.rb --no-verbose => false
+        - ./optparse.rb -v yes       => true
+        - ./optparse.rb              => false (or whatever is default)
+  EOD
+  opts.on('-v', '--[no-]verbose [FLAG]', TrueClass, 'Run verbosely') { |v|
+    options[:verbose] = v.nil? ? true : v
+  }
+}.parse!
+
+pp options
+
+puts "Run #{$0} --help to see hints" if ARGV.size == 0
